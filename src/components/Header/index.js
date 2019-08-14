a5086b54104e8284cbc31f65bf3dd8aa6de4859a
Ilya <ilya.meerov@gmail.com>
2019-08-13 22:11:34 -0700

    feat: modify layout and navigation

---
+++ src/components/Header/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'gatsby';
+import classNames from 'classnames';
+
+import styles from './styles.module.css'
+
+const Header = (props) => {
+  const {
+    location
+  } = props;
+
+  const isHomePage = location.pathname === '/'
+  const isBlogPage = location.pathname === '/blog';
+
+  const renderHeader = () => (
+    <nav className={classNames([styles.siteNav, isHomePage ? styles.homeNav : ''])}>
+      {!isHomePage && (
+        <div className={styles.homeLink}><Link to="/">Home</Link></div>
+      )}
+      <ul>
+        {!isBlogPage && (
+          <li><Link to="/blog">Blog</Link></li>
+        )}
+        <li>
+          <a href="https://github.com/Ilya-Meer/" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/ilya-meerovich/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+
+  return (
+    <div>
+      <header>
+        {renderHeader()}
+      </header>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,34 @@
+import '../../../static/stylesheets/normalize.css';
+import '../../../static/stylesheets/global.css';
+import React, { Fragment } from "react"
+import classNames from 'classnames';
+import styles from './styles.module.css';
+
+import Header from '../Header';
+
+class Layout extends React.Component {
+  render() {
+    const {
+      children,
+      location
+    } = this.props
+    
+    const isHomePage = location.pathname === '/'
+
+    return (
+      <Fragment>
+        <Header location={location} />
+        <main className={classNames({ [styles.homePage]: isHomePage })}>{children}</main>
+        {!isHomePage && (
+          <footer>
+            © {new Date().getFullYear()}.
+            {` `}
+            Ilya Meerovich
+          </footer>
+        )}
+      </Fragment>
+    )
+  }
+}
+
+export default Layout
