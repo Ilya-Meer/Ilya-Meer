979d8c12a13c3c1367c67a2419e84cedbf71ae4b
Ilya Meerovich <ilya.meerov@gmail.com>
2023-01-21 09:53:32 -0800

    fix: conditionally render canvas if height allows

--- src/components/Canvas/style.js
+++ src/components/Canvas/style.js
@@ -12,10 +12,14 @@ const fadeIn = keyframes`
 export const StyledCanvas = styled.canvas`
   width: 100vw !important;
   height: 20vh !important;
+  display: none;
   max-height: 20vh;
   position: absolute !important;
   bottom: 0;
   z-index: 1;
   opacity: 0;
   animation: ${fadeIn} 1s ease 0.25s 1 normal forwards;
+  @media all and (min-height: 568px) {
+    display: block;
+  }
 `;
