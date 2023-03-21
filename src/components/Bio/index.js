91e8fc7aea2f01f14521e53c4a14bfdaf54e1ae6
Ilya Meerovich <ilya.meerov@gmail.com>
2023-03-20 18:28:22 -0700

    fix: update bio

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -10,11 +10,6 @@ const Bio = ({ colours }) => {
       <h1>Hi there!</h1>
       <p>
         I'm Ilya Meerovich, a software engineer based in Vancouver, BC.
-        Currently, I work at{' '}
-        <a target="_blank" rel="noreferrer" href="https://www.coursehero.com">
-          Course Hero
-        </a>
-        , supporting seamless study experiences for students worldwide.
       </p>
       <p>
         Outside of work I enjoy reading, occasionally writing, and learning how
